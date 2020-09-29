14cc8e4272a189bb6233191482dc3bde027b2f2d
pgj234 <pgj94@naver.com>
2020-09-29 12:52:18 +0900

    commit

---
+++ Assets/Scripts/CamCtl.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCtl : MonoBehaviour {
+    Vector2?[] camPrevPos = {null, null};
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    // 씬 로드
+    public void SceneLoad(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // 휴대폰 뒤로가기 버튼 (종료)
+    void Update() {
+        if (Application.platform == RuntimePlatform.Android) {
+            if (Input.GetKey(KeyCode.Escape)) {
+                Application.Quit();
+                return;
+            }
+        }
+    }
+}
